ad1ba280f10a9cd3b1a677d3502ea949ac64d2e5
harissak <harissak_1991@hotmail.com>
2021-05-22 14:20:02 +0200

    add

---
+++ Application/Projet_SGBD_LUG-SAK/BL/Services_membre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BL
+{
+    class Services_membre
+    {
+
+        static public List<MBR> read_member_by_name(string memberName)
+        {
+            return DAL.AccesMBR.Read_MBR_BY_START_NAME(memberName);
+        }
+
+    }
+}
